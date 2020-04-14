8eeca8fb1f27bc5c858151054d8b641ed2fc720d
fb22 <4872297+fb22@users.noreply.github.com>
2020-04-14 21:42:57 +0200

    Update notify.py

--- homeassistant/components/llamalab_automate/notify.py
+++ homeassistant/components/llamalab_automate/notify.py
@@ -47,10 +47,7 @@ class AutomateNotificationService(BaseNotificationService):
 
         # Extract params from data dict
         data = dict(kwargs.get(ATTR_DATA) or {})
-        priority = data.get(ATTR_PRIORITY)
-
-        if priority not in ["normal", "high"] or priority is None:
-            priority = "normal"
+        priority = data.get(ATTR_PRIORITY,"normal")
 
         _LOGGER.debug("Sending to: %s, %s, prio: %s", self._recipient, str(self._device), priority)
 
